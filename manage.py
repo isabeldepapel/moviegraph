6b9c3418973dab8599d4f5f7e915bce95c7fa795
IsabelDePapel <isuchanek@gmail.com>
2018-01-02 11:22:37 -0800

    set up single settings file to use .env file with django-environ. renamed settings directory to keep in project files just in case

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviegraph.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviegraph.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
