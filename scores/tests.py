82daed77b3920660c80674fa8e0fab0c6c1e19ac
IsabelDePapel <isuchanek@gmail.com>
2018-01-02 15:17:29 -0800

    set up test file

--- scores/tests.py
+++ scores/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from .models import (
+    Title,
+    Genre,
+    TitleGenre,
+    Name,
+    Profession,
+    NameProfession,
+    Principal
+)
+
+
 # Create your tests here.
