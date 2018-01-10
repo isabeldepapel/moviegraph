a4a3195d9f1ecd16b6e0d9032351abffca019596
IsabelDePapel <isuchanek@gmail.com>
2018-01-09 23:44:02 -0800

    add write_to_csv and read_from_csv methods to spead up graph loading

--- scores/graph.py
+++ scores/graph.py
@@ -9,12 +9,18 @@ from .models import Title, Name, Principal
 from collections import deque
 
 import time
+import csv
+import environ
 
 KEVIN_BACON = Name.objects.filter(
     Q(primary_name='Kevin Bacon') &
     Q(birth_year=1958)
 )
 KEVIN_BACON_ID = 'nm0000102'
+
+DIR_PATH = environ.Path(__file__) - 1
+FILE_PATH = str(DIR_PATH.path('data/graph_info.csv'))
+
 
 
 def generate_graph():
@@ -29,65 +35,102 @@ def generate_graph():
     """
     start = time.time()
 
-    movies = Title.objects.filter(title_type='movie')
+    movies = Title.objects.filter(title_type='movie').values('id')
+    # print(movies)
 
     graph = {}
-
     # iterate through movies, find actors for each and create nodes
+    # for movie_id in movies.iterator():
     for movie in movies.iterator():
-        movie_id = movie.id
-        print(movie)
-        actors = Principal.objects.filter(title_id=movie_id)
-        costars = set(actors)
+        movie_id = movie['id']
+        # print(movie_id)
+        # print(movie)
+        actors = Principal.objects.filter(title_id=movie_id).values('name_id')
+        # print(actors)
+        # costars = set(actors)
+        costars = set([actor['name_id'] for actor in actors])
+        # print(costars)
 
         for actor in actors:
             # remove current actor from costars
-            costars.remove(actor)
+            actor_id = actor['name_id']
+            costars.remove(actor_id)
+            # print(actor_id)
+            # print(costars)
 
             # if no costars, go to next iteration
             if len(costars) < 1:
                 continue
 
-            if actor not in graph:
-                graph[actor] = costars
-            else:
-                graph[actor] = graph[actor] | costars
+            # if actor not in graph:
+            #     graph[actor] = costars
+            # else:
+            #     graph[actor] = graph[actor] | costars
+
             # if actor not in graph, add actor and its neighbors
             # with current movie as edge
 
-            # if actor not in graph:
-            #     graph[actor] = {costar: set([movie]) for costar in costars}
-            # else:
-            #     # check if costars are already neighbor nodes before adding
-            #     for costar in costars:
-            #         if costar not in graph[actor]:
-            #             graph[actor][costar] = set([movie])
-            #         else:
-            #             graph[actor][costar].add(movie)
+            if actor_id not in graph:
+                graph[actor_id] = {costar: set([movie_id]) for costar in costars}
+            else:
+                # check if costars are already neighbor nodes before adding
+                for costar in costars:
+                    if costar not in graph[actor_id]:
+                        graph[actor_id][costar] = set([movie_id])
+                    else:
+                        graph[actor_id][costar].add(movie_id)
 
             # add current actor back to costars
-            costars.add(actor)
+            costars.add(actor_id)
+
     print(time.time() - start)
     print('it took {0:0.1f} seconds'.format(time.time() - start))
     return graph
-    # iterate through actors, find costars for each movie
-    # for actor in actors:
-    #     # add actor node to dict
-    #     graph[actor] = {}
-    #     actors_movies = Principal.objects.filter(name_id=actor.id)
-    #
-    #     for movie in actors_movies:
-    #         costars = Principal.objects.filter(
-    #             Q(title_id=movie.id) &
-    #             ~Q(name_id=actor.id)
-    #         )
-    #
-    #         for costar in costars:
-    #             # check if costar already in graph
-    #             if costar in graph[actor]:
-    #                 graph[actor][costar].add(movie)
-    #             else:
-    #                 graph[actor][costar] = set([movie])
+
+
+def write_graph_to_csv(graph):
+    """Write graph info to a csv to read from later."""
+    start = time.time()
+
+    with open(FILE_PATH, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        for actor_id, neighbors in graph.items():
+            row = [actor_id]
+
+            for neighbor_id, movies in neighbors.items():
+                s = ''
+                s += neighbor_id + ','
+                s += ','.join(movie for movie in movies)
+                row.append(s)
+
+            writer.writerow(row)
+
+    print(time.time() - start)
+    print('it took {0:0.1f} seconds'.format(time.time() - start))
+
+
+def read_graph_from_csv(file_name=FILE_PATH):
+    """Create graph from csv file."""
+    start = time.time()
+    graph = {}
+
+    with open(file_name, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+
+        for row in reader:
+            actor_id = row[0]
+            neighbors = [col.split(',') for col in row[1:]]
+
+            graph[actor_id] = {}
+
+            for neighbor in neighbors:
+                costar_id = neighbor[0]
+                movie_ids = neighbor[1:]
+
+                graph[actor_id][costar_id] = set(movie_ids)
+
+    print('it took {0:0.1f} seconds'.format(time.time() - start))
+    return graph
 
 
 def bfs(graph, end, start=KEVIN_BACON_ID):
