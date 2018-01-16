b4ee728dfccc7c83c3c9821b08e5afe42d8dacea
IsabelDePapel <isuchanek@gmail.com>
2018-01-16 15:19:00 -0800

    add images to path display and add styling

--- scores/views.py
+++ scores/views.py
@@ -3,14 +3,20 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 from .graph import read_graph_from_csv, search_graph
+from .images import get_actor_image, get_movie_image
 from .models import Name
 from django.db.models import Q
 
 import string
 
-# GRAPH = read_graph_from_csv()
+testing = False
+# testing = True  # to test deploy
+if testing:
+    GRAPH = {}
+else:
+    GRAPH = read_graph_from_csv()
 # testing deploy
-GRAPH = {}
+# GRAPH = {}
 
 
 # helper function
@@ -24,13 +30,48 @@ def capitalize(text):
     # split on space and capitalize
     text = string.capwords(text)
     capitalized = ''
+    to_upper = False  # tracks whether letter needs to be capitalized
 
     # check for hyphen and apostrophe and capitalize
     # iterate through len - 1 because will capitalize at (i + 1)
-    for i in range(len(text) - 1):
-        capitalized += text[i]
+    for i in range(len(text)):
+        if to_upper:
+            capitalized += text[i].upper()
+        else:
+            capitalized += text[i]
+
         if text[i] == "-" or text[i] == "'":
-            pass
+            to_upper = True
+        else:
+            to_upper = False
+
+    return capitalized
+
+
+def get_images(path):
+    """
+    Get actor and movie images for the nodes and edges in the path.
+
+    Takes in a list of tuples (actor, movie) and returns a list of
+    dictionaries with actor and movies as the keys, and tuples (or
+    a list of tuples for movies) of the form (actor/movie, image) as
+    the values.
+    """
+    path_with_images = []
+
+    for step in path:
+        actor = step[0]
+        movies = step[1]
+
+        actor_info = (actor, get_actor_image(actor.primary_name))
+        movies_info = [
+            (movie, get_movie_image(movie.primary_title))
+            for movie in movies
+        ]
+
+        path_with_images.append({'actor': actor_info, 'movies': movies_info})
+
+    return path_with_images
 
 
 # Create your views here.
@@ -108,8 +149,11 @@ def submit(request):
         print(path)
         print(len(path))
 
-        context['path'] = path
-        context['search_for'] = actor
+        path_with_images = get_images(path)
+        context['path'] = path_with_images
+        # context['search_for'] = actor
+
+        context['path_end'] = (actor, get_actor_image(actor.primary_name))
 
         return render(request, 'scores/index.html', context)
 
