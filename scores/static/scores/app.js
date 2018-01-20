26da38278df61ceb811e97cd03092f435a17076f
IsabelDePapel <isuchanek@gmail.com>
2018-01-19 17:11:10 -0800

    edit app.js and views to use js to change app into a single-page app instead of multiple pages (avoids flickering when search graph, but slower to search)

--- scores/static/scores/app.js
+++ scores/static/scores/app.js
@@ -1,25 +1,5 @@
 const form = $('#search-graph-form');
 
-const checkName = function checkName(event) {
-  event.preventDefault();
-  $('.error').empty();
-
-  const url = form.attr('action');
-  // const formData = $('#search-for').val();
-  const formData = form.serialize();
-
-  console.log(formData)
-
-  $.get(url, formData, (response) => {
-    console.log('success!');
-    console.log(response);
-    console.log(response.actor_id);
-  }).fail(() => {
-    const html = '<p class="error">Not a valid name.</p>';
-    $('.error').append(html);
-  });
-};
-
 const hideOverlay = function hideOverlay(event) {
   const overlay = $(event.currentTarget).find('.overlay');
   overlay.hide();
@@ -29,11 +9,132 @@ const showOverlay = function showOverlay(event) {
   overlay.show();
 };
 
+const searchGraph = function searchGraph(findActorId) {
+  const searchUrl = '/submit';
+  const data = {
+    searchFor: findActorId,
+  };
+
+  $.get(searchUrl, data, (response) => {
+    const path = response.path
+    const endPath = response.path_end
+    const lenPath = path.length;
+
+    $('.score').append(`Bacon Score: ${lenPath}`);
+
+    let actorHtml;
+    let movieHtml;
+    let endHtml;
+
+    const anonImage = 'static/scores/images/profile.jpg';
+    const anonAlt = 'no photo available';
+
+    let imageSrc;
+    let imageAlt;
+
+    for (let i = 0; i < lenPath; i += 1) {
+      const [actorId, actorName, actorUrl] = path[i].actor;
+      const movies = path[i].movies;
+      const numMovies = movies.length;
+
+      if (actorUrl) {
+        imageSrc = actorUrl;
+        imageAlt = `photo of ${actorName}`;
+      } else {
+        imageSrc = anonImage;
+        imageAlt = anonAlt;
+      }
+
+      actorHtml = `
+      <li class="grid-x small-12 cell align-center">
+        <div class="image-container photo">
+          <a href="http://www.imdb.com/name/${actorId}" target="_blank">
+            <img src="${imageSrc}" alt="${imageAlt}" class="images profile-photos"/>
+            <div class="overlay grid-x align-middle">
+              <span class="overlay-text small-12 cell"><p>${actorName}</p></span>
+            </div>
+          </a>
+        </div>
+      </li>
+      <p class="connector">was in</p>
+      <ul class="movies grid-x small-12 cell align-center">
+      `;
+
+      movieHtml = '';
+      for (let j = 0; j < numMovies; j += 1) {
+        const [movieId, movieName, movieYear, movieUrl] = movies[j];
+
+        movieHtml += `
+          <li class="grid-x small-3 cell align-center">
+            <div class="image-container poster">
+              <a href="http://www.imdb.com/title/${movieId}" target="_blank">
+                <img src="${movieUrl}" alt="poster for ${movieName}" class="images posters"/>
+                <div class="overlay grid-x align-middle">
+                  <span class="overlay-text small-12 cell"><p>${movieName}, ${movieYear}</p></span>
+                </div>
+              </a>
+            </div>
+          </li>
+        `;
+      }
+      movieHtml += '</ul><p class="connector">with</p>';
+      $('ul.actors').append(actorHtml + movieHtml);
+    }
+
+    const [endId, endName, endUrl] = endPath;
+
+    if (endUrl) {
+      imageSrc = endUrl;
+      imageAlt = `photo of ${endName}`;
+    } else {
+      imageSrc = anonImage;
+      imageAlt = anonAlt;
+    }
+
+    endHtml = `
+      <li class="grid-x small-12 cell align-center">
+        <div class="image-container photo">
+          <a href="http://www.imdb.com/name/${endId}" target="_blank">
+            <img src="${imageSrc}" alt="${imageAlt}" class="images profile-photos"/>
+            <div class="overlay grid-x align-middle">
+              <span class="overlay-text small-12 cell"><p>${endName}</p></span>
+            </div>
+          </a>
+        </div>
+      </li>
+    `;
+
+    // const html = actorHtml + movieHtml + endHtml;
+    $('ul.actors').append(endHtml);
+  });
+};
+
+const resetDisplay = function resetDisplay() {
+  $('.score').empty();
+  $('ul.actors').empty();
+  $('.error').empty();
+};
+
+const checkName = function checkName(event) {
+  event.preventDefault();
+  resetDisplay();
+
+  const url = form.attr('action');
+  const formData = form.serialize();
+
+  $.get(url, formData, (response) => {
+    searchGraph(response.actor_id);
+  }).fail(() => {
+    const html = '<p class="error">Not a valid name.</p>';
+    $('.error').append(html);
+  });
+};
+
 $(document).ready(() => {
   // $(document).foundation();
 
   form.on('click', 'button', checkName);
-  
+
   $('.path').on('mouseenter', '.image-container', hideOverlay);
   $('.path').on('mouseleave', '.image-container', showOverlay);
 
--- scores/views.py
+++ scores/views.py
@@ -10,7 +10,7 @@ from django.db.models import Q
 # import string
 
 testing = False
-testing = True  # to test deploy
+# testing = True  # to test deploy
 if testing:
     GRAPH = {}
 else:
@@ -97,6 +97,39 @@ def get_images(path):
     return path_with_images
 
 
+def get_info(path):
+    """
+    Get actor and movie info for rendering as json.
+
+    Takes in list of tuples (actor, movie) and returns a list of
+    dictionaries with actor and movies as the keys, and tuples (or
+    list of tuples for movies) of the form (actor/movie name, movie
+    year, image) as the values.
+    """
+    path_with_images = []
+
+    for step in path:
+        actor = step[0]
+        movies = step[1]
+
+        actor_info = (
+            actor.id,
+            actor.primary_name,
+            get_actor_image(actor.primary_name))
+        movies_info = [
+            (
+                movie.id,
+                movie.primary_title,
+                movie.start_year,
+                get_movie_image(movie.primary_title)
+            )
+            for movie in movies
+        ]
+
+        path_with_images.append({'actor': actor_info, 'movies': movies_info})
+
+    return path_with_images
+
 # Create your views here.
 def index(request):
     """View function for main/home page."""
@@ -147,38 +180,91 @@ def validate(request):
         return JsonResponse(data)
 
 
+# def submit(request):
+#     print(request)
+#     print(request.GET)
+#     print(request.GET.get('search-for', default='test'))
+#     search_for = capitalize(request.GET.get('search-for', default=''))
+#     # search_for = capitalize(request.GET['search-for'])
+#     print(search_for)
+#     global GRAPH
+#     # filter for name in actors/actresses
+#     match = Name.objects.filter(
+#         Q(primary_name=search_for) &
+#         Q(birth_year__isnull=False) &
+#         (Q(professions__icontains='actor') |
+#          Q(professions__icontains='actress'))
+#     )
+#
+#     context = {}
+#     # check if no results
+#     if match.count() == 0 or match[0].id not in GRAPH:
+#         context['error_message'] = 'Not a valid name: ' + search_for
+#         return render(request, 'scores/index.html', context)
+#     else:
+#         actor = match[0]
+#         # graph = read_graph_from_csv()
+#         print(GRAPH[actor.id])
+#         path = search_graph(GRAPH, actor.id)
+#         print(path)
+#         print(len(path))
+#
+#         path_with_images = get_images(path)
+#         context['path'] = path_with_images
+#         # context['search_for'] = actor
+#
+#         context['path_end'] = (
+#             actor.id,
+#             actor,
+#             get_actor_image(actor.primary_name)
+#         )
+#
+#         return render(request, 'scores/index.html', context)
+
+    # return HttpResponse('This is a stub: ' + end_name)
+
 def submit(request):
-    search_for = request.GET.get('search_for', default='')
+    search_for = request.GET.get('searchFor', default='')
     # search_for = capitalize(request.GET['search-for'])
     print(search_for)
     global GRAPH
     # filter for name in actors/actresses
-    match = Name.objects.filter(
-        Q(primary_name=search_for) &
-        Q(birth_year__isnull=False) &
-        (Q(professions__icontains='actor') |
-         Q(professions__icontains='actress'))
+    # match = Name.objects.filter(
+    #     Q(primary_name=search_for) &
+    #     Q(birth_year__isnull=False) &
+    #     (Q(professions__icontains='actor') |
+    #      Q(professions__icontains='actress'))
+    # )
+    #
+    # context = {}
+    data = {}
+
+    # check if no results
+    # if match.count() == 0 or match[0].id not in GRAPH:
+    #     data['error_message'] = 'Not a valid name: ' + search_for
+    #     return render(request, 'scores/index.html', context)
+    # else:
+    actor = Name.objects.get(id=search_for)
+    # graph = read_graph_from_csv()
+    # print(GRAPH[actor.id])
+    path = search_graph(GRAPH, actor.id)
+    print(path)
+    print(len(path))
+
+    path_with_images = get_info(path)
+    print(path_with_images)
+    data['path'] = path_with_images
+    # context['search_for'] = actor
+
+    data['path_end'] = (
+        actor.id,
+        actor.primary_name,
+        get_actor_image(actor.primary_name)
     )
 
-    context = {}
-    # check if no results
-    if match.count() == 0 or match[0].id not in GRAPH:
-        context['error_message'] = 'Not a valid name: ' + search_for
-        return render(request, 'scores/index.html', context)
-    else:
-        actor = match[0]
-        # graph = read_graph_from_csv()
-        print(GRAPH[actor.id])
-        path = search_graph(GRAPH, actor.id)
-        print(path)
-        print(len(path))
-
-        path_with_images = get_images(path)
-        context['path'] = path_with_images
-        # context['search_for'] = actor
-
-        context['path_end'] = (actor, get_actor_image(actor.primary_name))
-
-        return render(request, 'scores/index.html', context)
+    # return render(request, 'scores/index.html', context)
+    jr = JsonResponse(data)
+    print(jr)
+    return jr
 
     # return HttpResponse('This is a stub: ' + end_name)
